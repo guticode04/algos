1edaa9fb8d5d1a32ea118bc1a2d9a02ba36c5cf2
Gustavo Gutierrez <gusguti04@gmail.com>
2020-06-03 21:15:44 -0700

    Complete binary search

---
+++ binary_search.js
@@ -0,0 +1,13 @@
+function binarySearch(array, target) {
+  if (array.length === 0) return -1;
+
+  const midpoint = Math.floor(array.length / 2);
+  if (array[midpoint] > target) {
+    return binarySearch(array.slice(0, midpoint), target);
+  } else if (array[midpoint] < target) {
+    const subResult = binarySearch(array.slice(midpoint + 1), target);
+    return subResult === -1 ? -1 : subResult + midpoint + 1;
+  } else {
+    return midpoint;
+  }
+}
