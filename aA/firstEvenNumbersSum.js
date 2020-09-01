53d6521f8c8c66df9070c4dc0600319d976da2c1
Gustavo Gutierrez <gusguti04@gmail.com>
2020-09-01 16:54:31 -0700

    Complete first even numbers sum

---
+++ aA/firstEvenNumbersSum.js
@@ -0,0 +1,9 @@
+// Write a function `firstEvenNumbersSum(n)` that returns the sum of the
+// first n even numbers recursively. Assume n > 0
+
+function firstEvenNumbersSum(n) {
+   if(n === 1) return 2;
+   return 2 * n + firstEvenNumbersSum(n - 1);
+}
+
+console.log(firstEvenNumbersSum(4));
