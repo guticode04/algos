05a21e37a3851e725e0f7fc585eec6ceb21557bc
Gustavo Gutierrez <gusguti04@gmail.com>
2020-08-31 19:57:31 -0700

    Complete exponent recursively

---
+++ aA/exponent.js
@@ -0,0 +1,12 @@
+// Write a function, `exponent(b, n)`, that calculates b^n recursively.
+// Your solution should accept negative values for n. Do NOT use ** or Math.pow
+
+function exponent(base, n) {
+   if(n === 0) return 1;
+
+   if(n > 0) {
+      return base * exponent(base, n - 1);
+   } else {
+      return 1/base * exponent(base, n + 1);
+   }
+}
