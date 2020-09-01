59ecd68def1c92d20e578b40b395e1d51b3c63ec
Gustavo Gutierrez <gusguti04@gmail.com>
2020-08-31 20:27:59 -0700

    Complete fibs sum with helper method

---
+++ aA/fibsSum.js
@@ -0,0 +1,17 @@
+// Write a function, `fibsSum(n)`, that finds the sum of the first n
+// fibonacci numbers recursively. Assume n > 0.
+// Note that for this problem, the fibonacci sequence starts with [1, 1]. 
+
+function fibsSum(n) {
+   if(n ===1) return 1;
+   if(n === 2) return 2;
+
+   return fibsSum(n - 1) +  fib(n);
+
+}
+
+function fib(n) {
+   if(n === 1) return 1;
+   if(n === 2) return 1;
+   return fib(n - 1) + fib(n - 2);
+}
