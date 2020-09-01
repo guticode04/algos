d5350447dd5d289091a0363c622fb73cb972039f
Gustavo Gutierrez <gusguti04@gmail.com>
2020-08-31 20:20:17 -0700

    Factors done

---
+++ aA/factors.js
@@ -0,0 +1,7 @@
+// Write a function, `factors(num)`, that returns an array containing the factors
+// of a number in ascending order.
+
+function factors(num) {
+   const facts = Array.from(Array(num).map( (ele, idx) => idx + 1);
+   return facts.filter( ele => num % ele === 0);
+}
