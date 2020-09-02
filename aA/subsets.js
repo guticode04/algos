16f1e96dd636eac3b41dfa8e9ca3ff45b467fdf0
Gustavo Gutierrez <gusguti04@gmail.com>
2020-09-01 20:06:24 -0700

    Add similar solution

--- aA/subsets.js
+++ aA/subsets.js
@@ -15,3 +15,14 @@ function subsets(arr) {
       return newArr;
    }));
 };
+
+//Same but somewhat different 
+
+function subset(arr) {
+   if (arr.length === 0) return [[]];
+
+   let first = arr[0];
+   let subs = subset(arr.slice(1));
+   let newSubs = subs.map((sub) => sub.concat([first]));
+   return subs.concat(newSubs);
+}
