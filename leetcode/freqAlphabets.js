84d98ebcad7ccdbf078a9675f623434a53154731
Gustavo Gutierrez <gusguti04@gmail.com>
2020-09-11 19:32:50 -0700

    Leet code problem frequent alpha

---
+++ leetcode/freqAlphabets.js
@@ -0,0 +1,16 @@
+function freqAlphabets(s) {
+   let alphabet = "abcdefghijklmnopqrstuvwxyz";
+   let string = "";
+
+   for (let i = 0; i < s.length; i++) {
+      let str = s[i];
+      let next = s[i + 2];
+      if (next === "#") {
+         string += alphabet[Number(s.slice(i, i + 2)) - 1]
+         i += 2;
+      } else {
+         string += alphabet[Number(str - 1)];
+      }
+   }
+   return string;
+}
