6ee3047fb9df7ac817d929eb5385f0e442cf284f
Gustavo Gutierrez <gusguti04@gmail.com>
2020-10-28 16:43:23 -0700

    Redo digital root with helper function to help with leetcode problem

--- digitalRoot.js
+++ digitalRoot.js
@@ -3,3 +3,22 @@
 function digitalRoot(num) {
   return num < 10 ? num : digitalRoot(digitalRoot(Math.floor(num / 10)) + (num % 10));
 }
+
+// Redo with helper function
+
+function digitalRoot(num) {
+  while (num > 10) {
+    num = digitalRootStep(num);
+  }
+  return num;
+}
+
+function digitalRootStep(num) {
+  let root = 0;
+  while (num > 0) {
+    root += num % 10;
+    num = Math.floor(num/10);
+  }
+  return root;
+}
+
